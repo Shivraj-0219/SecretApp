c2cfaa15f0186b18c191a1d47dc65797b8204c91
SHIVRAJ MUTTAGI <57551826+Shivraj-0219@users.noreply.github.com>
2020-05-13 21:25:43 +0530

    Auth 2.0 encyrption

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use(passport.session());
 
 
 
-//mongoose.connect("mongodb://localhost:27017/userDB", { useNewUrlParser: true });
+
 mongoose.connect(process.env.MONGO_ATLAS,
  { useCreateIndex: true, useNewUrlParser: true, useUnifiedTopology: true })
  .then(() => console.log('connected to DB'))
